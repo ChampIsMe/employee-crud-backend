2144b08144fd695773e726f14e62e79bcca0439a
ChampIsMe <okinyiphilip@gmail.com>
2024-02-07 17:55:28 +0300

    Project setup

---
+++ src/test/java/com/phil/employeecrudbackend/EmployeeCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phil.employeecrudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudBackendApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
